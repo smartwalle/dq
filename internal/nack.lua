3a89ea376b191b30d81d2e21d843820403dfbd9e
SmartWalle <smartwalle@gmail.com>
2024-09-27 14:45:56 +0800

    脚本调整 - nack.lua

--- internal/nack.lua
+++ internal/nack.lua
@@ -2,36 +2,37 @@
 -- KEYS[2] - 待重试队列
 -- KEYS[3] - MessageKey(uuid)
 
-local key = KEYS[3]
-local score = redis.call('ZSCORE', KEYS[1], key)
+local mKey = KEYS[3]
+-- 从[处理中队列]获取消息的分值，主要用于判断该消息是否还存在于[处理中队列]中
+local score = redis.call('ZSCORE', KEYS[1], mKey)
 if (not score) then
     return ''
 end
 
 -- 判断消息结构是否存在
-local exists = redis.call('EXISTS', key)
-if (exists == 0) then
+local found = redis.call('EXISTS', mKey)
+if (found == 0) then
     return ''
 end
 
 -- 清除消费者id
-redis.call('HSET', key, 'c', '')
+redis.call('HSET', mKey, 'c', '')
 
 -- 获取消息 uuid
-local uuid = redis.call('HGET', key, 'uuid')
+local uuid = redis.call('HGET', mKey, 'uuid')
 
 -- 获取剩余重试次数
-local count = redis.call('HGET', key, 'rc')
+local count = redis.call('HGET', mKey, 'rc')
 if count ~= nil and count ~= '' and tonumber(count) > 0 then
     -- 剩余重试次数大于 0
     -- 更新剩余重试次数
-    redis.call('HINCRBY', key, 'rc', -1)
+    redis.call('HINCRBY', mKey, 'rc', -1)
     -- 添加到[待重试队列]中
-    redis.call('RPUSH', KEYS[2], key)
+    redis.call('RPUSH', KEYS[2], mKey)
 else
     -- 删除[消息结构]
-    redis.call('DEL', key)
+    redis.call('DEL', mKey)
 end
 -- 从[处理中队列]中删除消息
-redis.call('ZREM', KEYS[1], key)
+redis.call('ZREM', KEYS[1], mKey)
 return uuid
