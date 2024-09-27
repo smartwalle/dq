4b24304839764340f888867ecca7576069d73b8e
SmartWalle <smartwalle@gmail.com>
2024-09-27 14:48:49 +0800

    脚本调整 - schedule.lua

--- internal/schedule.lua
+++ internal/schedule.lua
@@ -1,11 +1,12 @@
  -- KEYS[1] - 延迟队列
  -- KEYS[2] - 消息结构
- -- ARGV[1] - 消息 id
- -- ARGV[2] - 消息 uuid
+ -- ARGV[1] - 消息 id (id)
+ -- ARGV[2] - 消息 uuid (uuid)
  -- ARGV[3] - 消费时间
- -- ARGV[4] - 队列名称
- -- ARGV[5] - 消息内容
- -- ARGV[6] - 剩余重试次数
+ -- ARGV[4] - 队列名称 (qn)
+ -- ARGV[5] - 消息内容 (pl)
+ -- ARGV[6] - 剩余重试次数 (rc)
+ -- 投递时间 (dt)
 
 -- 添加到[延迟队列]
 redis.call('ZADD', KEYS[1], ARGV[3], ARGV[1])
