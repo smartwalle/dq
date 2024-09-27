4f30c6f5ad4dc64fea8e3680f66baf15c94940d4
SmartWalle <smartwalle@gmail.com>
2024-09-27 14:45:07 +0800

    脚本调整 - clear_consumer.lua

--- internal/clear_consumer.lua
+++ internal/clear_consumer.lua
@@ -1,8 +1,8 @@
 -- KEYS[1] - 消费者队列
 
 -- 获取当前时间
-local time = redis.call('TIME')
-local milliseconds = time[1] * 1000 + math.floor(time[2] / 1000)
+local now = redis.call('TIME')
+local milliseconds = now[1] * 1000 + math.floor(now[2] / 1000)
 
 -- 从[消费者队列]删除超时的消费者
 redis.call('ZREMRANGEBYSCORE', KEYS[1], '-inf', milliseconds)
