11064c03e629e4b0a55f3c403e53f42f2dddd74e
SmartWalle <smartwalle@gmail.com>
2024-09-27 14:44:45 +0800

    脚本调整 - active_to_retry.lua

--- internal/active_to_retry.lua
+++ internal/active_to_retry.lua
@@ -2,10 +2,10 @@
 -- KEYS[2] - 待重试队列
 -- KEYS[3] - 消费者队列
 
-local doRetry = function(key, now)
+local doRetry = function(mKey, now)
     -- 判断消息结构是否存在
-    local exists = redis.call('EXISTS', key)
-    if (exists == 0) then
+    local found = redis.call('EXISTS', mKey)
+    if (found == 0) then
         return
     end
 
@@ -15,41 +15,41 @@ local doRetry = function(key, now)
         local timeout = tonumber(consumerTimeout)
         if (timeout > now) then
             -- 如果消费者的有效时间大于当前时间，则更新[处理中队列]中消息的消费超时时间
-            redis.call('ZADD', KEYS[1], timeout, key)
+            redis.call('ZADD', KEYS[1], timeout, mKey)
             return
         end
     end
 
     -- 获取剩余重试次数
-    local count = redis.call('HGET', key, 'rc')
+    local count = redis.call('HGET', mKey, 'rc')
     if (count ~= nil and count ~= '' and tonumber(count) > 0) then
         -- 剩余重试次数大于 0
         -- 更新剩余重试次数
-        redis.call('HINCRBY', key, 'rc', -1)
+        redis.call('HINCRBY', mKey, 'rc', -1)
         -- 添加到[待重试队列]中
-        redis.call('RPUSH', KEYS[2], key)
+        redis.call('RPUSH', KEYS[2], mKey)
     else
         -- 删除[消息结构]
-        redis.call('DEL', key)
+        redis.call('DEL', mKey)
         -- TODO 记录失败消息
     end
     -- 清除消费者id
-    redis.call('HSET', key, 'c', '')
+    redis.call('HSET', mKey, 'c', '')
     -- 从[处理中队列]中删除消息
-    redis.call('ZREM', KEYS[1], key)
+    redis.call('ZREM', KEYS[1], mKey)
 end
 
 -- 获取当前时间
-local time = redis.call('TIME')
-local milliseconds = time[1] * 1000 + math.floor(time[2] / 1000)
+local now = redis.call('TIME')
+local milliseconds = now[1] * 1000 + math.floor(now[2] / 1000)
 
 -- 获取[处理中队列]中已经消费超时的消息
-local keys = redis.call('ZRANGEBYSCORE', KEYS[1], '-inf', milliseconds)
-if (#keys > 0) then
-    for _, key in ipairs(keys) do
-        if (key ~= nil and key ~= '') then
+local mKeys = redis.call('ZRANGEBYSCORE', KEYS[1], '-inf', milliseconds)
+if (#mKeys > 0) then
+    for _, mKey in ipairs(mKeys) do
+        if (mKey ~= nil and mKey ~= '') then
             -- 重试处理逻辑
-            doRetry(key, milliseconds)
+            doRetry(mKey, milliseconds)
         end
     end
 end
