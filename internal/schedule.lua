9de3622607e0dc168bd8c5dca3c7fd2679f1d8c7
SmartWalle <smartwalle@gmail.com>
2024-09-29 08:36:45 +0800

    重试逻辑调整

--- internal/schedule.lua
+++ internal/schedule.lua
@@ -12,6 +12,6 @@
 redis.call('ZADD', KEYS[1], ARGV[3], ARGV[1])
 -- 获取当前时间
 local now = redis.call('TIME')
-local seconds = tonumber(now[1])
+local second = tonumber(now[1])
 -- 写入消息结构
-redis.call('HMSET', KEYS[2], 'id', ARGV[1], 'uuid', ARGV[2], 'qn', ARGV[4], 'pl', ARGV[5], 'rc', ARGV[6], 'dt', seconds)
+redis.call('HMSET', KEYS[2], 'id', ARGV[1], 'uuid', ARGV[2], 'qn', ARGV[4], 'pl', ARGV[5], 'rc', ARGV[6], 'dt', second)
